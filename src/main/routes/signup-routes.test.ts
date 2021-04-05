2d4d172c7d1f6fb264b897e450bedebd6b86e7c1
marcolongitude <marcocpdti@gmail.com>
2021-04-04 22:29:03 -0300

    refactor: connect to mongodb before integration test

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,15 +1,32 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 describe('SignUp Routes', () => {
+
+    beforeAll( async () => {
+        beforeAll( async () => {
+            await MongoHelper.connect(process.env.MONGO_URL)
+        })
+    })
+
+    afterAll( async () => {
+        await MongoHelper.disconnect()
+    })
+
+    beforeEach( async () => {
+        const accountCollection = MongoHelper.getCollection('accounts')
+        accountCollection.deleteMany({})
+    })
+
     test('Should return an account on success', async () => {
         await request(app)
             .post('/api/signup')
             .send({
-                name: 'Rodrigo',
-                email: 'rodrigo.manguinho@gmail.com',
-                password: '123',
-                passwordConfirmation: '123'
+                name: 'Marco Aurélio',
+                email: 'marco@gmail.com',
+                password: '123456',
+                passwordConfirmation: '123456'
             })
             .expect(200)
     })
